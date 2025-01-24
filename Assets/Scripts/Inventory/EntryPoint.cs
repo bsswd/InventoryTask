4ee9353fb21e646bb2dfe5496fb63d0cd087471c
bsswd <alexandersinkin@mail.ru>
2025-01-24 13:52:13 +0700

    ExampleTest

---
+++ Assets/Scripts/Inventory/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Inventory.Data;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        public InventoryGridView _view;
+
+        private void Start()
+        {
+            var inventoryData = new InventoryGridData()
+            {
+                Size = new Vector2Int(6, 5),
+                OwnerId = "Player",
+                Slots = new List<InventorySlotData>()
+            };
+
+
+            var size = inventoryData.Size;
+            for (var i = 0; i < size.x; i++)
+            {
+                for (var j = 0; j < size.y; j++)
+                {
+                    var index = i * size.y + j;
+                    inventoryData.Slots.Add(new InventorySlotData());
+                }
+            }
+
+            
+            
+            var slotData = inventoryData.Slots[0];
+            slotData.ItemId = "Medkit";
+            slotData.Amount = 50;
+
+            var inventory = new InventoryGrid(inventoryData);
+            
+            _view.Setup(inventory);
+        }
+    }
+}
--- Assets/Scripts/Inventory/InventoryGridView.cs
+++ Assets/Scripts/Inventory/InventoryGridView.cs
@@ -9,6 +9,16 @@ namespace Inventory
         {
             var slots = inventory.GetSlots();
             var size = inventory.Size;
+            
+            for (var i = 0; i < size.x; i++)
+            {
+                for (var j = 0; j < size.y; j++)
+                {
+                    var slot = slots[i, j];
+                    
+                    print($"Slot ({i}:{j}). Item: {slot.ItemId}, Amount: {slot.Amount}");
+                }
+            }
         }
     }
 }
