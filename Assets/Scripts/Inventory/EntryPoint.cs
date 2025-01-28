d95c3b135f0f8e9b3687fc13fb81a928816561f7
bsswd <alexandersinkin@mail.ru>
2025-01-28 10:44:19 +0700

    Update EntryPoint.cs

--- Assets/Scripts/Inventory/EntryPoint.cs
+++ Assets/Scripts/Inventory/EntryPoint.cs
@@ -1,21 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using Inventory.Controllers;
 using Inventory.Data;
+using Inventory.View;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Inventory
 {
     public class EntryPoint : MonoBehaviour
     {
+        [SerializeField] private ScreenView _screenView;
         
+        private const string OWNER_1 = "Player";
+        private const string OWNER_2 = "Container";
+
+        private readonly string[] _itemIds =
+            { "Helmet", "Pistol ammo", "Assault rifle ammo", "Medkit", "Jacket", "Cap", "Armor vest" };
+
         
         private InventoryService _inventoryService;
+        private ScreenController _screenController;
+
+        private string _cachedOwnerId;
 
         private void Start()
         {
             _inventoryService = new InventoryService();
+
+            var inventoryDataPlayer = CreateTestInventory(OWNER_1);
+            _inventoryService.RegisterInventory(inventoryDataPlayer);
             
+            var inventoryDataContainer = CreateTestInventory(OWNER_2);
+            _inventoryService.RegisterInventory(inventoryDataContainer);
+
+            _screenController = new ScreenController(_inventoryService, _screenView);
+            _screenController.OpenInventory(OWNER_1);
+            _cachedOwnerId = OWNER_1;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _screenController.OpenInventory(OWNER_1);
+                _cachedOwnerId = OWNER_1;
+            }
             
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                _screenController.OpenInventory(OWNER_2);
+                _cachedOwnerId = OWNER_2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                var rIndex = Random.Range(0, _itemIds.Length);
+                var rItemId = _itemIds[rIndex];
+                var rAmount = Random.Range(1, 50);
+
+                var result = _inventoryService.AddItemsToInventory(_cachedOwnerId, rItemId, rAmount);
+                
+                print($"Item added: {rItemId}. Amount added: {result.ItemsAddedAmount}");
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                var rIndex = Random.Range(0, _itemIds.Length);
+                var rItemId = _itemIds[rIndex];
+                var rAmount = Random.Range(1, 50);
+
+                var result = _inventoryService.RemoveItems(_cachedOwnerId, rItemId, rAmount);
+                
+                print($"Item removed: {rItemId}. Trying to remove: {result.ItemsToRemoveAmount}, Success: {result.Success}");
+            }
         }
 
         private InventoryGridData CreateTestInventory(string ownerId)
